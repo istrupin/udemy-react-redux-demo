3926c13f55459865fda714be0990d8d3e9c01a88
Igor Strupinskiy <igor.strupinskiy@gmail.com>
2017-02-18 19:54:28 +0000

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar.jsx';
+import VideoList from './components/video_list.jsx';
+import YTSearch from 'youtube-api-search';
+
+
+const API_KEY = 'AIzaSyDDtZfmjlfEBm_K-29N8zkFKIwDCJxTInA';
+
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            console.log(videos);
+            this.setState({videos});
+        });
+        
+    }
+    
+    render(){
+        return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos} />
+        </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
